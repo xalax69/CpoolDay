d46ceedd1f481f7709bece9cacb37eb08cea910d
xalax69 <167294297+xalax69@users.noreply.github.com>
2024-04-17 10:11:19 +0000

    exercice 2

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -3,7 +3,7 @@ int print_char(char);
 void my_print_ascii(void)
 {
     int j;
-    for(j = 64; j <= 127; j++) {
+    for(j = 32; j <= 127; j++) {
         print_char((char)j);
     }
 }
---
+++ my_print_n_ascii.c
@@ -0,0 +1,16 @@
+int print_char(char);
+
+void my_print_n_ascii(int howMany)
+{
+    int j;
+    int f = howMany + 32;
+    for(j = 32; j <=f ; j++)
+    {
+        print_char((char)j);
+    }
+}
+//int main(void)
+//{
+//    my_print_n_ascii(5);
+//    return 0;
+//}
