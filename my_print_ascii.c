97131ff6a03ff5cbe1f36b1b08e324fb00ba6b9e
xalax69 <167294297+xalax69@users.noreply.github.com>
2024-04-17 12:52:15 +0000

    [ADD]

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -3,13 +3,13 @@ int print_char(char);
 void my_print_ascii(void)
 {
     int j;
-    for(j = 33; j <= 127; j++) {
+    for(j = 33; j <= 126; j++) {
         print_char((char)j);
     }
 }
 
-// int main(void)
-// {
-//     my_print_ascii();
-//     return 0;
-// }
+int main(void)
+{
+    my_print_ascii();
+    return 0;
+}
